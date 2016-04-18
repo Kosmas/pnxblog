0ad4ecfaca8a306a0c17a14426661e6b2f7075a3
Kosmas Chatzimichalis <kosmas@mach7x.com>
2016-04-18 22:55:34 +0200

    add changeset definition

--- web/controllers/session_controller.ex
+++ web/controllers/session_controller.ex
@@ -1,7 +1,8 @@
 defmodule Pnxblog.SessionController do
   use Pnxblog.Web, :controller
+  alias Pnxblog.User
 
   def new(conn, _params) do
-    render conn, "new.html"
+    render conn, "new.html", changeset: User.changeset(%User{})
   end
 end
